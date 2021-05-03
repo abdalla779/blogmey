17a289bbbbce779da90f2320299bac4d60e5ef7b
abdalla779 <abdallaelmagwery@yahoo.com>
2021-05-03 15:42:04 +0200

    configuring db blog model

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=2000)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(upload_to='pics')
+
+    def __str__(self):
+        return self.title
